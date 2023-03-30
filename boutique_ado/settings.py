1daf9d8526e01a28f2dbe5171feadc2275d0cf51
STANNIDITSANNI <Ditsanstas@gmail.com>
2023-03-30 12:56:43 +0000

    Set debug to False

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*r)ksjtz(l3dt994*1&ax0a&_ffazr7(l5p!b=!p-#%sq(p4f0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['boutique-ado-shop.herokuapp.com', 'localhost']
 
